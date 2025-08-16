8f9b03d2b15f5d5b8d1bd7b683395a7f3f8dd136
lucaidau <dotoan270806@gmail.com>
2025-08-17 01:40:01 +0700

    Add Responsive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const share_btn = document.getElementById("share_btn");
 const share_popup = document.getElementById("share_popup");
+
 share_btn.addEventListener("click", () => {
   share_popup.classList.toggle("show");
 });
